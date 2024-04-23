491c44c9605b10a7f1028b2fb68a78c0111a5152
joey <148897320+strdustcrusader@users.noreply.github.com>
2024-04-23 18:37:03 -0400

    box.cpp

--- box.cpp
+++ box.cpp
@@ -77,13 +77,7 @@ int main() {
 	int img[MAX_H][MAX_W];
 	int h, w;
 
-	readImage(img, h, w); // read it from the file "inImage.pgm"
-	// h and w were passed by reference and
-	// now contain the dimensions of the picture
-	// and the 2-dimensional array img contains the image data
-
-	// Now we can manipulate the image the way we like
-	// for example we copy its contents into a new array
+	readImage(img, h, w); 
 	int out[MAX_H][MAX_W];
 
 	for (int row = 0; row < h; row++) {
@@ -96,6 +90,5 @@ int main() {
 		}
 	}
 
-	// and save this new image to file "outImage.pgm"
 	writeImage(out, h, w);
 }
